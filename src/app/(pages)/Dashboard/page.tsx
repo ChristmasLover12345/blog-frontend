16d9d3c2bad34eec70082f0123a10569b9265989
ChristmasLover12345 <madnes0205@gmail.com>
2025-04-02 10:54:14 -0700

    a lil bit more logic

--- src/app/(pages)/Dashboard/page.tsx
+++ src/app/(pages)/Dashboard/page.tsx
@@ -21,91 +21,70 @@ import {
 } from "flowbite-react";
 
 import React, { useEffect, useState } from "react";
-import BlogEntries from "@/utils/BlogEntries.json"
+import BlogEntries from "@/utils/BlogEntries.json";
 
 const page = () => {
   const [openModal, setOpenModal] = useState<boolean>(false);
   // These useStates ill be for our form
-  const[blogTitle, setBlogTitle] = useState<string>("")
-    const [blogImage, setBlogImage] = useState<any>()
-    const [blogDescription, setBlogDescription] = useState<string>("")
-    const [blogCategory, setblogCategory] = useState<string>("")
-    const [blogId, setBlogId] = useState<number>()
-    const [blogUserId, setBlogUserId] = useState<number>()
-    const [blogPublisherName, setBlogPublisherName] = useState<string>("")
+  const [blogTitle, setBlogTitle] = useState<string>("");
+  const [blogImage, setBlogImage] = useState<any>();
+  const [blogDescription, setBlogDescription] = useState<string>("");
+  const [blogCategory, setblogCategory] = useState<string>("");
+  const [blogId, setBlogId] = useState<number>();
+  const [blogUserId, setBlogUserId] = useState<number>();
+  const [blogPublisherName, setBlogPublisherName] = useState<string>("");
 
-    const [edit, setEdit] = useState<boolean>(false)
+  const [edit, setEdit] = useState<boolean>(false);
 
-    const [blogItems, setBlogItems] = useState<IBlogItems[]>(BlogEntries)
+  const [blogItems, setBlogItems] = useState<IBlogItems[]>(BlogEntries);
 
-    useEffect(() => {
-
-      if(!checkToken())
-      {
-
-      }
-      else
-      {
-
-      }
-
-    }, []);
-
-    // Form Functions
-    const handleTitle = (e: React.ChangeEvent<HTMLInputElement>) => setBlogTitle(e.target.value);
-
-    const handleDescription = (e:  React.ChangeEvent<HTMLInputElement>) => setBlogDescription(e.target.value);
-
-    const handleCategory = (categories: string) => setblogCategory(categories);
-
-    const handleImage = (e:  React.ChangeEvent<HTMLInputElement>) => {
-
+  useEffect(() => {
+    if (!checkToken()) {
+    } else {
     }
+  }, []);
 
-    // Accordion Functions
+  // Form Functions
+  const handleTitle = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setBlogTitle(e.target.value);
 
-    const handleShow = () => {
-      setOpenModal(true);
-      setEdit(false)
-  }
-    const handleEdit = (items: IBlogItems) => {
-      setOpenModal(true);
-      setEdit(true)
+  const handleDescription = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setBlogDescription(e.target.value);
+
+  const handleCategory = (categories: string) => setblogCategory(categories);
+
+  const handleImage = (e: React.ChangeEvent<HTMLInputElement>) => {};
+
+  // Accordion Functions
+
+  const handleShow = () => {
+    setOpenModal(true);
+    setEdit(false);
+  };
+  const handleEdit = (items: IBlogItems) => {
+    setOpenModal(true);
+    setEdit(true);
+  };
+
+  const handlePublish = async (items: IBlogItems) => {
+    items.isPublished = !items.isPublished;
+  };
+
+  const handleDelete = async (items: IBlogItems) => {
+    items.isDeleted = true;
+  };
+
+  // Save Function
+
+  const handleSave = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const item = {};
+
+    setOpenModal(false);
+
+    if (edit) {
+    } else {
     }
-
-    const handlePublish = async (items: IBlogItems) => {
-      items.isPublished = !items.isPublished
-    }
-
-    const handleDelete = async (items: IBlogItems) => {
-      items.isDeleted = true;
-    }
-
-    // Save Function
-
-    const handleSave = (e: React.MouseEvent<HTMLButtonElement>) => {
-
-      const item = {}
-
-      setOpenModal(false);
-
-      if(edit)
-      {
-
-      }
-      else
-      {
-
-      }
-
-    }
-
-
-
-
-
-
-
+  };
 
   return (
     <main className="flex min-h-screen flex-col p-24">
@@ -114,7 +93,7 @@ const page = () => {
 
         <Button onClick={() => setOpenModal(true)}>AddBlog</Button>
         <Modal show={openModal} onClose={() => setOpenModal(false)}>
-          <ModalHeader>{edit ? 'Edit Blog Post' : "Add Blog Post"}</ModalHeader>
+          <ModalHeader>{edit ? "Edit Blog Post" : "Add Blog Post"}</ModalHeader>
           <ModalBody>
             <form className="flex max-w-md flex-col gap-4">
               <div>
@@ -145,9 +124,15 @@ const page = () => {
               <div>
                 <div className="flex items-center gap-2">
                   <Dropdown label="Categories" dismissOnClick={true}>
-                    <DropdownItem onClick={() => handleCategory("Jujitsu")}>Jujitsu</DropdownItem>
-                    <DropdownItem onClick={() => handleCategory("Jajutsu")}>Jajutsu</DropdownItem>
-                    <DropdownItem onClick={() => handleCategory("PooJitsu")}>PooJitsu</DropdownItem>
+                    <DropdownItem onClick={() => handleCategory("Jujitsu")}>
+                      Jujitsu
+                    </DropdownItem>
+                    <DropdownItem onClick={() => handleCategory("Jajutsu")}>
+                      Jajutsu
+                    </DropdownItem>
+                    <DropdownItem onClick={() => handleCategory("PooJitsu")}>
+                      PooJitsu
+                    </DropdownItem>
                   </Dropdown>
                 </div>
                 <div className="mb-2 block">
@@ -163,7 +148,7 @@ const page = () => {
             </form>
           </ModalBody>
           <ModalFooter>
-            <Button onClick={() => handleSave}>           Save and publish           </Button>
+            <Button onClick={() => handleSave}> Save and publish </Button>
             <Button onClick={() => handleSave}>Save</Button>
             <Button color="gray" onClick={() => setOpenModal(false)}>
               Cancel
@@ -171,40 +156,83 @@ const page = () => {
           </ModalFooter>
         </Modal>
         <Accordion alwaysOpen className="w-3xl mt-5">
-            <AccordionPanel>
-              <AccordionTitle>Published Blog Items</AccordionTitle>
-              <AccordionContent>
-                <ListGroup>
-
-                  {blogItems.map((item: IBlogItems, index: number) => {
-                    return (
-                      <div key={index}>
-                        {
-                          item.isPublished && !item.isDeleted && (
-                            <div className="flex flex-col p-10">
-                              <h2 className="text-3xl">{item.title}</h2>
-                              <div className="flex flex-row space-x-3">
-                                <button color="blue">Edit</button>
-                                <button color="red">Delete</button>
-                                <button color="yellow">Unpublish</button>
-                              </div>
-                            </div>
-                          )
-                        }
-                      </div>
-                    )
-                  })}
-
-                </ListGroup>
-              </AccordionContent>
-            </AccordionPanel>
-            <AccordionPanel>
-              <AccordionTitle>Unpublished Blog Items</AccordionTitle>
-              <AccordionContent>
-                <ListGroup></ListGroup>
-              </AccordionContent>
-            </AccordionPanel>
-          </Accordion>
+          <AccordionPanel>
+            <AccordionTitle>Published Blog Items</AccordionTitle>
+            <AccordionContent>
+              <ListGroup>
+                {blogItems.map((item: IBlogItems, index: number) => {
+                  return (
+                    <div key={index}>
+                      {item.isPublished && !item.isDeleted && (
+                        <div className="flex flex-col p-10">
+                          <h2 className="text-3xl">{item.title}</h2>
+                          <div className="flex flex-row space-x-3">
+                            <button
+                              onClick={() => handleEdit(item)}
+                              color="blue"
+                            >
+                              Edit
+                            </button>
+                            <button
+                              onClick={() => handleDelete(item)}
+                              color="red"
+                            >
+                              Delete
+                            </button>
+                            <button
+                              onClick={() => handlePublish(item)}
+                              color="yellow"
+                            >
+                              Unpublish
+                            </button>
+                          </div>
+                        </div>
+                      )}
+                    </div>
+                  );
+                })}
+              </ListGroup>
+            </AccordionContent>
+          </AccordionPanel>
+          <AccordionPanel>
+            <AccordionTitle>Unpublished Blog Items</AccordionTitle>
+            <AccordionContent>
+              <ListGroup>
+                {blogItems.map((item: IBlogItems, index: number) => {
+                  return (
+                    <div key={index}>
+                      {item.isPublished && !item.isDeleted && (
+                        <div className="flex flex-col p-10">
+                          <h2 className="text-3xl">{item.title}</h2>
+                          <div className="flex flex-row space-x-3">
+                            <button
+                              onClick={() => handleEdit(item)}
+                              color="blue"
+                            >
+                              Edit
+                            </button>
+                            <button
+                              onClick={() => handleDelete(item)}
+                              color="red"
+                            >
+                              Delete
+                            </button>
+                            <button
+                              onClick={() => handlePublish(item)}
+                              color="yellow"
+                            >
+                              Unpublish
+                            </button>
+                          </div>
+                        </div>
+                      )}
+                    </div>
+                  );
+                })}
+              </ListGroup>
+            </AccordionContent>
+          </AccordionPanel>
+        </Accordion>
       </div>
     </main>
   );
